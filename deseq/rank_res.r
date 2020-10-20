c83bfab139199b714f775f39028c99f9b3f6a8be
Qing Yang <qyang.kevin@gmail.com>
2020-10-20 17:07:09 -0600

    shortened gene list via padj filter

--- deseq/rank_res.r
+++ deseq/rank_res.r
@@ -1,10 +1,17 @@
-setwd("./")
+setwd("~/Documents/infection_netowrk_meta_analysis/")
+library(Biobase)
 
 lfc_mat=read.table("./deseq/res/meta_log2FC_mat_2019.txt",header = T)
 padj_mat=read.table("./deseq/res/meta_padj_mat_2019.txt",header = T)
 
 lfc_mat[is.na(lfc_mat)]=0
 padj_mat[is.na(padj_mat)]=1
+
+keep=which(rowMin(as.matrix(padj_mat))<0.01) 
+
+
+lfc_mat=lfc_mat[keep,]
+padj_mat=padj_mat[keep,]
 
 rnk_mat=matrix(NA, ncol = dim(lfc_mat)[2], nrow = dim(lfc_mat)[1])
 rownames(rnk_mat)=rownames(lfc_mat)
@@ -15,4 +22,4 @@ for (i in seq(ncol(lfc_mat))){
 
 colnames(rnk_mat)=c("DENV1","DENV2","HSV1","EBOLA","HRV","HIV","RSV","STAPH","IAV","HCMV")
 
-write.table(rnk_mat,"./deseq/res/rank_matrix.txt",row.names = T, col.names = T, quote = F, sep = "\t")
+write.table(rnk_mat,"./deseq/res/rank_matrix_padj_cutoff_001.txt",row.names = T, col.names = T, quote = F, sep = "\t")
